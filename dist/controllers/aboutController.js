582d19d4dd238c3261d26e9d6cc89b7b4e9d260d
trojan03 <alymbek.sadybakasov@gmai.com>
2018-04-27 00:04:50 +0200

    one more

---
+++ dist/controllers/aboutController.js
@@ -0,0 +1,10 @@
+var App;
+(function (App) {
+    var AboutController = /** @class */ (function () {
+        function AboutController() {
+        }
+        return AboutController;
+    }());
+    App.AboutController = AboutController;
+})(App || (App = {}));
+//# sourceMappingURL=aboutController.js.map
